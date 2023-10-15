9d411e90ac21111bce85ff9b0e24c3064482a692
Mohamed-ben-Ali <muhammed1197ali@gmail.com>
2023-10-15 07:43:44 +0200

    First Commit - Update button not working correctely

---
+++ JS/index.js
@@ -0,0 +1,69 @@
+let productName = document.getElementById("productName");
+let productPrice = document.getElementById("productPrice");
+let productCat = document.getElementById("productCat");
+let productDesc = document.getElementById("productDesc");
+let productList = [];
+
+if (localStorage.getItem("List") != null) {
+    productList = JSON.parse(localStorage.getItem("List"))
+    // console.log(productList);
+    displayProduct(productList);
+} else {
+    productList = [];
+}
+
+
+function AddProduct() {
+    let product = {
+        name: productName.value,
+        price: productPrice.value,
+        category: productCat.value,
+        description: productDesc.value
+    }
+    productList.push(product);
+    setToLocalStorage()
+    displayProduct(productList);
+    ClearForm();
+
+}
+
+function displayProduct(List) {
+    let Box = ``;
+    for (let i = 0; i < List.length; i++) {
+
+        // console.log(List[i])
+        Box += `  <tr>
+        <td>${i + 1}</td>
+        <td>${List[i].name}</td>
+        <td>${List[i].price}</td>
+        <td>${List[i].category}</td>
+        <td>${List[i].description}</td>
+
+        <td><button class="btn btn-warning ">Update</button></td>
+        <td><button class="btn btn-danger" onClick="deleteProduct(${i})">Delete</button></td>
+
+    </tr>`
+    }
+    document.getElementById("tableData").innerHTML = Box // Add new row to table
+}
+
+function ClearForm() {
+    productName.value = " ";
+    productPrice.value = " ";
+    productCat.value = " ";
+    productDesc.value = " ";
+}
+
+function deleteProduct(index) {
+    productList.splice(index, 1);
+    // console.log(productList);
+    displayProduct(productList);
+    setToLocalStorage();
+}
+function setToLocalStorage() {
+    localStorage.setItem("List", JSON.stringify(productList))
+}
+
+// function getProductData(index) {
+//     console.log(productList[index])
+// }
